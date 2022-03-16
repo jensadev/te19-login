16fa8674d0fc8bb46110a38d65450e367ede6df7
Jens Andreasson <jens.andreasson@ntig.se>
2022-03-16 11:18:00 +0100

    ✨ login and profile func.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const pool = require('../utils/database');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index.njk', {
         title: 'Home',
         layout: 'layout.njk',
+    });
+});
+
+router.get('/gethash/:pwd', function (req, res, next) {
+    const pwd = req.params.pwd;
+    bcrypt.hash(pwd, 10).then(function (hash) {
+        res.json({ hash });
     });
 });
 
@@ -16,25 +25,57 @@ router.get('/login', function (req, res, next) {
     });
 });
 
-router.post('/login', function (req, res, next) {
-    res.json({fix: 'fix'});
+router.post('/login', async (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        return res.redirect('/login');
+    }
+
+    await pool
+    .promise()
+    .query('SELECT id, name, password FROM users WHERE name = ? LIMIT 1', [username])
+    .then(([rows]) => {
+        bcrypt.compare(password, rows[0].password).then((result) =>{
+            // result == true
+            if (result) {
+                req.session.uid = rows[0].id;
+                req.session.username = rows[0].name; 
+                res.redirect('/profile');
+            } else {
+                res.render('login.njk', {
+                    title: 'Failed login',
+                    layout: 'layout.njk',
+                });
+            }
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
 });
 
 router.get('/logout', function (req, res, next) {
-    res.json({fix: 'fix'});
+    res.json({ fix: 'fix' });
 });
 
 router.get('/register', function (req, res, next) {
-    res.json({fix: 'fix'});
+    res.json({ fix: 'fix' });
 });
 
 router.post('/register', function (req, res, next) {
-    res.json({fix: 'fix'});
+    res.json({ fix: 'fix' });
 });
 
-router.get('/profile', function (req, res, next) {
+router.get('/profile', (req, res, next) =>{
+    if (!req.session.uid && !req.session.username) {
+        return res.redirect('/login');
+    }
+
     res.render('secret.njk', {
         title: 'Secret',
+        user: req.session.username,
         layout: 'layout.njk',
     });
 });
