962b32aa6b567962100b2181250cd3ae8bec032d
Jens Andreasson <jens.andreasson@ntig.se>
2022-04-01 08:52:56 +0200

    🎨 check

--- routes/index.js
+++ routes/index.js
@@ -37,6 +37,10 @@ router.post('/login', async (req, res, next) => {
     .promise()
     .query('SELECT id, name, password FROM users WHERE name = ? LIMIT 1', [username])
     .then(([rows]) => {
+        if(rows.length === 0) {
+            console.log("hantera no user found");
+            return res.redirect('/login');
+        };
         bcrypt.compare(password, rows[0].password).then((result) =>{
             // result == true
             if (result) {
@@ -44,6 +48,7 @@ router.post('/login', async (req, res, next) => {
                 req.session.username = rows[0].name; 
                 res.redirect('/profile');
             } else {
+                console.log("password fel, men det berättar vi inte för anv");
                 res.render('login.njk', {
                     title: 'Failed login',
                     layout: 'layout.njk',
@@ -57,7 +62,9 @@ router.post('/login', async (req, res, next) => {
 });
 
 router.get('/logout', function (req, res, next) {
-    res.json({ fix: 'fix' });
+    req.session.destroy();
+    console.log(req.session);
+    res.redirect('/');
 });
 
 router.get('/register', function (req, res, next) {
